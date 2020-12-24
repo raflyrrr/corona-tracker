8e8894ad19601690e774fc4a3c0017e8034334d6
Rafli Ramadhan <raflyr44@gmail.com>
2020-12-24 20:11:13 +0700

    final corona tracker

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Footer = () => {
+  return (
+    <footer>
+      <hr />
+      <div>
+        Copyright <i class="far fa-copyright"></i> By Rafli Ramadhan
+      </div>
+    </footer>
+  );
+};
+export default Footer;
